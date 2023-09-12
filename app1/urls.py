e72fa454f9a8a58182bc58def972390d86af9ef4
arjun <arjunchaudhary@gmail.com>
2023-09-12 12:00:24 +0530

    push

--- app1/urls.py
+++ app1/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('product-filter/<int:id>/',productfilter),
     path('register/',register),
     path('login/',login,name='login'),
+    path('logout/',logout,name='logout'),
     path('product_details/<int:id>/',product_details,name='productdetails'),
-    path('contact',Contact_us)
+    path('contact',Contact_us),
+    path('profile',profile,name='profile'),
+    path('order',Order,name='order')
 ]
